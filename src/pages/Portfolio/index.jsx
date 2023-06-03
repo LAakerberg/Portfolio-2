452103b8a58ea330128e862b511a93ba7ed03fed
Linus Aakerberg <44141432+LAakerberg@users.noreply.github.com>
2023-06-03 17:35:41 +0200

    Added data to project and portfolio page

--- src/pages/Portfolio/index.jsx
+++ src/pages/Portfolio/index.jsx
@@ -1,9 +1,30 @@
+import { Cards } from '../../components/Cards';
+import { portfolio } from '/src/data/portfolioData'; // Import the portfolio data directly
+
 export function Portfolio() {
+  // Uses the imported portfolio data instead of making an API call
+  const data = portfolio;
+  const isLoading = false; // Since you're not making an API call, set isLoading to false
+  const isError = false; // Since you're not making an API call, set isError to false
+
+  /* console.log(data); */
+
+  if (isLoading) {
+    return <div>Loading</div>;
+  }
+
+  if (isError) {
+    return <div>Error</div>;
+  }
+
   return (
     <>
-      <div className="flex pt-6">
+      <div className="flex flex-col pt-6">
         <div>
-          <p>Hello Portfolio me</p>
+          <h3 className="pb-4">Portfolio</h3>
+        </div>
+        <div>
+          <Cards project={data} showInProgress={false} />
         </div>
       </div>
     </>
--- src/pages/Project/index.jsx
+++ src/pages/Project/index.jsx
@@ -1,11 +1,52 @@
+import { Cards } from '../../components/Cards';
+import { portfolio } from '/src/data/portfolioData'; // Import the portfolio data directly
+
 export function Project() {
+  // Uses the imported portfolio data instead of making an API call
+  const data = portfolio;
+  const isLoading = false; // Since you're not making an API call, set isLoading to false
+  const isError = false; // Since you're not making an API call, set isError to false
+
+  /* console.log(data); */
+
+  if (isLoading) {
+    return <div>Loading</div>;
+  }
+
+  if (isError) {
+    return <div>Error</div>;
+  }
+
   return (
     <>
-      <div className="flex pt-6">
+      <div className="flex flex-col pt-6">
         <div>
-          <p>Hello Project me</p>
+          <h3 className="pb-4">Projects</h3>
+        </div>
+        <div>
+          <Cards project={data} showInProgress={true} />
         </div>
       </div>
     </>
   );
 }
+
+/* function FetchData() {
+  const { data, response, isLoading, isError } = useApiCall(
+    '/src/data/portfolioData.js',
+    'GET'
+  );
+
+  console.log(response);
+  console.log(data);
+
+  if (isLoading) {
+    return <div>Loading</div>;
+  }
+
+  if (isError) {
+    return <div>Error</div>;
+  }
+
+  return <div>Data loaded</div>;
+} */
