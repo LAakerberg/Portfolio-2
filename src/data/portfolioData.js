36cdc7389780a1b37e4c9db2f45e6a391c7ec598
Linus Aakerberg <44141432+LAakerberg@users.noreply.github.com>
2023-06-03 17:34:32 +0200

    Updated data with projects

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -7,8 +7,8 @@ export const portfolio = [
     descriptions: `This was my first bigger project during my studies as a front-end developer.
                         The first edition was only made with HTML & CSS. The site has been developed and there is now
                         some Javascript included in the programming`,
-    imgThumb: '/image/Rainydays-onedesign.jpg',
-    imgHigh: '/image/Rainydays-high.jpg',
+    imgThumb: '/src/assets/img/portfolio/Rainydays-onedesign.jpg',
+    imgHigh: '/src/assets/img/portfolio/Rainydays-high.jpg',
     url: 'https://incandescent-biscochitos-c68324.netlify.app/',
     urlGithub: 'https://github.com/LAakerberg/cross-course-cms-ca',
     completed: true,
@@ -22,8 +22,8 @@ export const portfolio = [
     descriptions: `This was my second project during my studies as a front-end developer.
                         This is a site for CSM were kids, school classes and parents go visit the museum for educations.
                         The site as been built with HTML & CSS`,
-    imgThumb: '/image/CMS.jpg',
-    imgHigh: '/image/CMS-high.jpg',
+    imgThumb: '/src/assets/img/portfolio/CMS.jpg',
+    imgHigh: '/src/assets/img/portfolio/CMS-high.jpg',
     url: 'https://dazzling-cray-677b8f.netlify.app',
     urlGithub: 'https://github.com/LAakerberg/Semester-Project-1',
     completed: true,
@@ -37,8 +37,8 @@ export const portfolio = [
     descriptions: `This is my third project were we should build an blog and do API request to a backend that's was setup
                         with wordpress. The design is built as a headless.
                         The site is build with HTML, CSS & JavaScript`,
-    imgThumb: '/image/OneOnlyblog.jpg',
-    imgHigh: '/image/OneOnlyblog-high.jpg',
+    imgThumb: '/src/assets/img/portfolio/OneOnlyblog.jpg',
+    imgHigh: '/src/assets/img/portfolio/OneOnlyblog-high.jpg',
     url: 'https://effulgent-crostata-065cc5.netlify.app',
     urlGithub:
       'https://github.com/Noroff-FEU-Assignments/project-exam-1-Nattlegend',
@@ -52,106 +52,83 @@ export const portfolio = [
     slug: 'Onedesign',
     descriptions: `This is my own portfolio site were you will find all of my completed/on-going and up-coming projects.
                         Here will I try to collect all of my libirayrs.`,
-    imgThumb: '/image/emile-perron-xrVDYZRGdw4-unsplash-640.jpg',
-    imgHigh: '/image/emile-perron-xrVDYZRGdw4-unsplash-1920.jpg',
+    imgThumb:
+      '/src/assets/img/portfolio/emile-perron-xrVDYZRGdw4-unsplash-640.jpg',
+    imgHigh:
+      '/src/assets/img/portfolio/emile-perron-xrVDYZRGdw4-unsplash-1920.jpg',
     url: '',
     urlGithub: '',
-    completed: false,
-    inProgress: true,
+    completed: true,
+    inProgress: false,
   },
   {
     id: 4,
-    title: '',
+    title: 'Semester Project 2',
     name: 'New project',
     slug: '',
     descriptions: `This is an upcoming project.`,
-    imgThumb: '/image/alexander-sinn-KgLtFCgfC28-unsplash-640.jpg',
-    imgHigh: '/image/alexander-sinn-KgLtFCgfC28-unsplash-1920.jpg',
+    imgThumb:
+      '/src/assets/img/portfolio/alexander-sinn-KgLtFCgfC28-unsplash-640.jpg',
+    imgHigh:
+      '/src/assets/img/portfolio/alexander-sinn-KgLtFCgfC28-unsplash-1920.jpg',
     url: '',
-    urlGithub: '',
+    urlGithub: 'https://github.com/LAakerberg/semester_project_ca',
     completed: false,
-    inProgress: true,
+    inProgress: false,
   },
   {
     id: 5,
-    title: '',
+    title: 'JavaScript Frameworks CA',
     name: 'Hard core programming',
     slug: '',
     descriptions: `This is an upcoming project.`,
     imgThumb: '',
     imgHigh: '',
-    url: '',
+    url: 'https://meek-pika-e025e2.netlify.app/',
     urlGithub: '',
     completed: false,
-    inProgress: false,
+    inProgress: true,
   },
   {
     id: 6,
-    title: '',
+    title: 'Project Exam 2',
     name: 'New project',
     slug: '',
     descriptions: `This is an upcoming project.`,
-    imgThumb: '/image/alexander-sinn-KgLtFCgfC28-unsplash-640.jpg',
-    imgHigh: '/image/alexander-sinn-KgLtFCgfC28-unsplash-1920.jpg',
-    url: '',
-    urlGithub: '',
-    completed: false,
+    imgThumb:
+      '/src/assets/img/portfolio/alexander-sinn-KgLtFCgfC28-unsplash-640.jpg',
+    imgHigh:
+      '/src/assets/img/portfolio/alexander-sinn-KgLtFCgfC28-unsplash-1920.jpg',
+    url: 'https://holidaze-summer.netlify.app/',
+    urlGithub: 'https://github.com/LAakerberg/Holidaze-project-CA',
+    completed: true,
     inProgress: false,
   },
   {
     id: 7,
-    title: '',
-    name: 'Onedesign Portfolio',
-    slug: 'Onedesign',
-    descriptions: `This is my own portfolio site were you will find all of my completed/on-going and up-coming projects.
-                        Here will I try to collect all of my libirayrs.`,
-    imgThumb: '/image/emile-perron-xrVDYZRGdw4-unsplash-640.jpg',
-    imgHigh: '/image/emile-perron-xrVDYZRGdw4-unsplash-1920.jpg',
+    title: 'Portfolio 2',
+    name: 'Onedesign Portfolio 2',
+    slug: 'Onedesign, Portfolio',
+    descriptions: `Refactoring my old Portfolio site from regular HTML, CSS & Vanilla JavaScript to now include Vite.js as bundler and a dev server with React.js library, added TailwindCSS as a CSS Framework. `,
+    imgThumb:
+      '/src/assets/img/portfolio/emile-perron-xrVDYZRGdw4-unsplash-640.jpg',
+    imgHigh:
+      '/src/assets/img/portfolio/emile-perron-xrVDYZRGdw4-unsplash-1920.jpg',
     url: '',
     urlGithub: '',
     completed: false,
-    inProgress: false,
+    inProgress: true,
   },
   {
     id: 8,
-    title: '',
-    name: 'The one and only blog',
-    slug: 'One and only',
-    descriptions: `This is my third project were we should build an blog and do API request to a backend that's was setup
-                        with wordpress. The design is built as a headless.
-                        The site is build with HTML, CSS & JavaScript`,
-    imgThumb: '/image/OneOnlyblog.jpg',
-    imgHigh: '',
-    url: '',
-    urlGithub: '',
-    completed: false,
-    inProgress: false,
-  },
-  {
-    id: 9,
-    title: '',
-    name: 'Community Science Museum',
-    slug: 'CSM',
-    descriptions: `This was my second project during my studies as a front-end developer.
-                        This is a site for CSM were kids, school classes and parents go visit the museum for educations.
-                        The site as been built with HTML & CSS`,
-    imgThumb: '/image/CMS.jpg',
-    imgHigh: '',
-    url: '',
-    urlGithub: '',
-    completed: false,
-    inProgress: false,
-  },
-  {
-    id: 10,
-    title: '',
-    name: 'Rainydays',
-    slug: '',
-    descriptions: `This was my first bigger project during my studies as a front-end developer.
-                        The first edition was only made with HTML & CSS. The site has been developed and there is now
-                        some Javascript included in the programming`,
-    imgThumb: '/image/Rainydays-onedesign.jpg',
-    imgHigh: '',
+    title: 'New project',
+    name: 'New project',
+    slug: 'Onedesign',
+    descriptions: `This is an upcoming project`,
+    imgThumb:
+      '/src/assets/img/portfolio/emile-perron-xrVDYZRGdw4-unsplash-640.jpg',
+    imgHigh:
+      '/src/assets/img/portfolio/emile-perron-xrVDYZRGdw4-unsplash-1920.jpg',
     url: '',
     urlGithub: '',
     completed: false,
