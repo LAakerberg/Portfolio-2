26183884916eb6cdfcbf058fcb5bd49c03838879
Linus Aakerberg <44141432+LAakerberg@users.noreply.github.com>
2023-06-03 17:37:17 +0200

    Added func so links closes hamburger menu

--- src/layout/Navigation/index.jsx
+++ src/layout/Navigation/index.jsx
@@ -1,20 +1,30 @@
 import { Link } from 'react-router-dom';
 
-export function Nav() {
+export function Nav(toggleMenu) {
+  const closeMenu = toggleMenu;
+
   return (
     <nav className="nav_link text-3xl">
       <ul>
         <li className="">
-          <Link to="/">Home</Link>
+          <Link to="/" onClick={closeMenu}>
+            Home
+          </Link>
         </li>
         <li className="">
-          <Link to="/portfolio">Portfolio</Link>
+          <Link to="/portfolio" onClick={closeMenu}>
+            Portfolio
+          </Link>
         </li>
         <li className="">
-          <Link to="/project">Project</Link>
+          <Link to="/project" onClick={closeMenu}>
+            Project
+          </Link>
         </li>
         <li className="">
-          <Link to="/about">About</Link>
+          <Link to="/about" onClick={closeMenu}>
+            About
+          </Link>
         </li>
       </ul>
     </nav>
