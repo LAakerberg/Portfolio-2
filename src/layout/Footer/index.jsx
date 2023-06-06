50b295fdf22b698eac16cf0b6e4cdd2b91470ce3
Linus Aakerberg <44141432+LAakerberg@users.noreply.github.com>
2023-06-07 00:26:47 +0200

    Updated fonts and removed footer.css style.

--- src/layout/Footer/index.jsx
+++ src/layout/Footer/index.jsx
@@ -3,8 +3,10 @@ export function Footer() {
     <>
       <footer className="bg-slate-500 border-t border-slate-300 h-20 m-auto text-center rounded-b-xl">
         <div className="">
-          <p className="text-white uppercase text-xl footer_text">Onedesign</p>
-          <p className="text-white text-base footer_memo">
+          <p className="text-white uppercase text-xl font-['Squada-One']">
+            Onedesign
+          </p>
+          <p className="text-white text-base font-['Merienda-Regular']">
             &copy; 2023 Onedesign
           </p>
         </div>
